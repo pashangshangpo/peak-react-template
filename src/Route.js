49af9bc3143eb2a52ef152613950a6c2e4433345
爬上上坡 <pashangshangpo@gmail.com>
2018-11-26 21:12:45 +0800

    更新模板

---
+++ src/Route.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { HashRouter } from 'react-router-dom'
+import { Route, Switch } from 'react-router'
+
+import Home from './router/home'
+
+class Routes extends React.Component {
+  renderRouters() {
+    return El(Switch,
+      {},
+      El(Route,
+        {
+          exact: true,
+          path: '/',
+          component: Home,
+        }
+      )
+    )
+  }
+
+  render() {
+    return El(HashRouter,
+      {},
+      this.renderRouters()
+    )
+  }
+}
+
+export default Routes
